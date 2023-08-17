669bebcc8fde5ee533c67e6518c7aae0c4087d94
Jimin Ha <smjmjh26@naver.com>
2023-08-17 16:21:27 +0900

    first commit

---
+++ src/main/java/com/example/blobproject/BlobprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.blobproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlobprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlobprojectApplication.class, args);
+	}
+
+}
